1dc4bdd8121646cf7d0092d916260c9e51e5d32c
Kiall Mac Innes <kiall@macinnes.ie>
2026-03-01 09:23:47 +0000

    Fix Device Discovery

--- custom_components/unifi_sms_gateway/entity.py
+++ custom_components/unifi_sms_gateway/entity.py
@@ -5,7 +5,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.helpers.device_registry import CONNECTION_NETWORK_MAC, DeviceInfo
 
 from .coordinator import UnifiSMSGatewayCoordinator
-from .const import DOMAIN, LOGGER
+from .const import CONF_NAME, DEFAULT_NAME, DOMAIN
 
 class UnifiSMSGatewayEntity(CoordinatorEntity[UnifiSMSGatewayCoordinator]):
     _attr_has_entity_name = True
@@ -19,10 +19,12 @@ class UnifiSMSGatewayEntity(CoordinatorEntity[UnifiSMSGatewayCoordinator]):
         """Return the device info."""
 
         # Determine Device Identifiers based on MAC address(es) returned by the API
-        identifiers: set[tuple[str, str]] = set()
+        connections: set[tuple[str, str]] = set()
         for mac in self.coordinator.data.mac:
-            identifiers.add((CONNECTION_NETWORK_MAC, mac))
+            connections.add((CONNECTION_NETWORK_MAC, mac))
 
         return DeviceInfo(
-            identifiers=identifiers,
+            name=self.coordinator.config_entry.data.get(CONF_NAME) or DEFAULT_NAME,
+            identifiers={(DOMAIN, self.coordinator.config_entry.entry_id)},
+            connections=connections,
         )
